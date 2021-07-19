446d80cd390286d75e2dc612ef3db326ea6cf745
SaminNouri <79563118+SaminNouri@users.noreply.github.com>
2021-07-19 13:57:38 +0430

    Add files via upload

---
+++ SaveInfo.py
@@ -0,0 +1,13 @@
+from pymongo import *
+
+class SaveInfo:
+
+
+
+    def saveToDataBase(self,users,cluster,database,collection):
+        self.client=MongoClient(cluster)
+        self.database=self.client[database]
+        self.collection=self.database[collection]
+        self.collection.insert_many(users)
+
+
